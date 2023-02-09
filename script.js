55c64710630abb4f5512c6164dffb91d755f49d1
Amir Kooshky <kooshkya@gmail.com>
2023-02-09 19:37:41 +0330

    main: add player name and move input features and output player move for debug

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert("hello world!");
+// Troghout the code, rocks will be represented by the number 1, paper by the number 2, and scissors by the number 3.
+
+let name = prompt("Welcome to RPS! Please enter your name:");
+let playerScore = 0, computerScore = 0, roundNumber = 1;
+const winScore = 3;
+while (playerScore < winScore && computerScore < winScore) {
+    let playerMove = prompt(`Round ${roundNumber}, please enter your move:`);
+    switch (playerMove.charAt(0).toLowerCase()) {
+        case '1':
+        case 'r':
+            playerMove = 1;
+            break;
+        case '2':
+        case 'p':
+            playerMove = 2;
+            break;
+        case '3':
+        case 's':
+            playerMove = 3;
+            break;
+        default:
+            playerMove = -1;
+            break;
+    }
+
+    if (playerMove === -1) {
+        alert("Invalid input, try again.");
+        continue;
+    }
+
+    alert(`Player Move: ${playerMove}`)
+}
